2e28e5a49886985dd93a5de0ce0d7ebf5f33a020
godu <duarte.goncalves@aeis.ch>
2019-10-01 07:47:57 +0200

    Initial commit, created Project with Spring Initializer

---
+++ whitejack/src/main/java/ch/toast/whitejack/WhitejackApplication.java
@@ -0,0 +1,13 @@
+package ch.toast.whitejack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitejackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitejackApplication.class, args);
+	}
+
+}
